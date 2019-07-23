2d7a3c0f84c69b2ef0c6cfb29ffbb07a4f626d13
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2019-07-23 04:11:03 -0700

    Register user endpoint

--- server.js
+++ server.js
@@ -17,18 +17,25 @@ const importFromJson = jsonFile => {
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const routingRules = importFromJson('./routes.json');
-const userDb = importFromJson('./users.json');
+let userDb = importFromJson('./users.json');
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults());
 server.use(jsonParser);
 
 const SECRET_KEY = process.env.SECRET_KEY;
-const expiresIn = '1h';
+const ACCESS_EXPIRES_IN  = '30m';
+const REFRESH_EXPIRES_IN = '2h';
 
 // Token functions
-const createToken = payload => {
-  return jwt.sign(payload, SECRET_KEY, {expiresIn});
+const createAccessToken = payload => {
+  const accessPayload = { ...payload, token_type: 'ACCESS' };
+  return jwt.sign(accessPayload, SECRET_KEY, {expiresIn: ACCESS_EXPIRES_IN});
+}
+
+const createRefreshToken = payload => {
+  const refreshPayload = { ...payload, token_type: 'REFRESH' };
+  return jwt.sign(refreshPayload, SECRET_KEY, {expiresIn: REFRESH_EXPIRES_IN});
 }
 
 const verifyToken = token => {
@@ -38,6 +45,20 @@ const verifyToken = token => {
 const findUser = email => {
   const userIndex = userDb.users.findIndex(user => user.email === email);
   return (userIndex !== -1) ? { ...userDb.users[userIndex] } : null;
+}
+
+const addUser = ({name, email, password}) => {
+  const id = userDb.users.length + 1;
+  const newUser = { id, name, email, password }
+  const newUserDb = {
+    users: [ ...userDb.users, newUser ],
+  }
+
+  fs.writeFile(
+    './users.json', JSON.stringify(newUserDb, null, 4), 'UTF-8',
+    () => { userDb = importFromJson('./users.json'); }
+  );
+  return newUser;
 }
 
 const isAuthenticated = ({email, password}) => {
@@ -60,18 +81,55 @@ server.post('/auth/login', (req, res) => {
   }
   // if the code reaches this point you know they're authenticated
   const { name } = findUser(email);
-  const access_token = createToken({email, password});
+  const access_token = createAccessToken({ email, password });
+  const refresh_token = createRefreshToken({ email, password });
   res.status(200).json({
     user: { name, email },
-    access_token }
-  );
+    access_token,
+    refresh_token,
+  });
 })
 
-const getUrlBits = (url) => {
-  return url.split('/').filter(s => s !== "");
-}
+// POST /auth/login endpoint
+server.post('/auth/signup', (req, res) => {
+  const {name, email, password} = req.body;
+
+  if (findUser(email)) {
+    const status = 400;
+    const message = 'Email taken already';
+    res.status(status).json({status, message});
+    return;
+  }
+
+  const user = addUser({name, email, password});
+  res.status(200).json({ user });
+})
+
+// POST /auth/refresh endpoint
+server.post('/auth/refresh', (req, res) => {
+  const refreshToken = verifyToken(req.body.refresh_token);
+
+  if (refreshToken.token_type !== 'REFRESH' || refreshToken instanceof Error) {
+    const status = 401;
+    const message = 'Error: refresh_token is not valid';
+    res.status(status).json({status, message});
+    return;
+  }
+
+  const { email, password } = refreshToken;
+  const newAccessToken = createAccessToken({email, password});
+  const newRefreshToken = createRefreshToken({email, password})
+
+  res.status(200).json({
+    access_token: newAccessToken,
+    refresh_token: newRefreshToken,
+  });
+})
 
 server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  const getUrlBits = (url) => {
+    return url.split('/').filter(s => s !== "");
+  }
   const urlBits = getUrlBits(req.originalUrl);
 
   const gettingSingularProduct = (
@@ -101,6 +159,7 @@ server.use(/^(?!\/auth).*$/, (req, res, next) => {
   
   const access_token = req.headers.authorization.split(' ')[1];
   const tokenObj = verifyToken(access_token);
+
   if (tokenObj instanceof Error) {
     const status = 401;
     const message = 'Error: access_token is not valid';
